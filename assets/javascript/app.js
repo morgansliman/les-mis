de939a1097416221ba6bb04a2d1b1733b125c185
morgansliman <morgansliman@gmail.com>
2016-11-06 19:22:15 -0500

    center & right nav tabs now toggle header styles for easier editing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,6 +7,33 @@ $(document).ready(function() {
 			}
 
 			$(this).addClass('active');
+
+			switch ($(this).text()) {
+				case 'Left':
+
+				break;
+
+				case 'Center':
+					$('header').children().eq(0).attr(
+						'src', 'assets/images/layer-title.png'
+					).removeClass('header-title2').addClass('header-title');
+					$('header').children().eq(1).attr(
+						'src', 'assets/images/layer-border-texture-2.png'
+					).removeClass('header-pic2').addClass('header-pic');
+					$('header').removeClass('header');
+				break;
+
+				case 'Right':
+					console.log($('header').children());
+					$('header').children().eq(0).attr(
+						'src', 'assets/images/title.png'
+					).removeClass('header-title').addClass('header-title2');
+					$('header').children().eq(1).attr(
+						'src', 'assets/images/cutout-shadow.png'
+					).removeClass('header-pic').addClass('header-pic2');
+					$('header').addClass('header');
+				break;
+			}
 		}
 	});
 });
